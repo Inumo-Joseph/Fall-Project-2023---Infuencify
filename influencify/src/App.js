37b4a6e85dd671e492278cafd7b6c1a5ace58f0f
Joseph <inumoj@gmail.com>
2023-11-15 03:13:18 -0500

    Small changes t App.js and Firebase

--- influencify/src/App.js
+++ influencify/src/App.js
@@ -4,22 +4,15 @@ import React from 'react'
 import {useState, useEffect} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./Signup";
-import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch, Navigate} from "react-router-dom";
 import Login from './Login'
 import Home from './Home'
 import UserSettings from './User'
 import Main from './components/Main';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import React, { useEffect, useState } from 'react';
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Navigate,
-} from 'react-router-dom';
+
 import { auth } from './Config/firebase-config';
-import Login from './Routes/Login';
 
 
 
--- influencify/src/Config/firebase-config.js
+++ influencify/src/Config/firebase-config.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
